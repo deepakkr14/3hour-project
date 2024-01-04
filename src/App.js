393a36b91e5bc3cae8595ba304fd260508653880
deepakkr14 <deepakbipin142@gmail.com>
2024-01-04 10:20:48 +0530

    readme.md

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import Form from './component/form '
+import Form from "./component/form ";
 import React, { useState } from "react";
 
-import Items from './component/items'
+import Items from "./component/items";
 function App() {
   let [UserData, setData] = useState([]);
-  const Products=(data)=>{
-
-      setData((pdata) => {
-        return [...pdata, data];
-      });
-}
-const delhandler=(idToDelete)=>{
-UserData = UserData.filter(item => { 
-  return item.orderid !== idToDelete})
-setData(UserData)
-localStorage.removeItem(idToDelete)
-}
+  const Products = (data) => {
+    setData((pdata) => {
+      return [...pdata, data];
+    });
+  };
+  const delhandler = (idToDelete) => {
+    UserData = UserData.filter((item) => {
+      return item.orderid !== idToDelete;
+    });
+    setData(UserData);
+    localStorage.removeItem(idToDelete);
+  };
   return (
     <>
-   <Form datafn={Products} />
-   <Items  data={UserData} del={delhandler}/>
-   </>
-  )
+      <Form datafn={Products} />
+      <Items data={UserData} del={delhandler} />
+    </>
+  );
 }
 
 export default App;
